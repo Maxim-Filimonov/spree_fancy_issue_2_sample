5af1ccb59a7694a2078b5bc65005adbe23bd24c6
Maxim Filimonov <tpaktopsp@gmail.com>
2013-01-28 21:36:55 +1100

    Initial commit after running rails new spree_fancy_issue_2 -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeFancyIssue2::Application.config.secret_token = '5b6c46d9c879ccf079c63aded6a3b2fbd829bfe8b14375a0e451d2c627fb0d9b265b7f9df63236b85709d7709cfdf9deea3bf67237dad2ace4dfb1cd836d216e'
